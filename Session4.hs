c079ee7355894844aad58e2da430c37729d958e3
fluffy-unicorn <matthijsvhelden@gmail.com>
2017-05-09 17:18:57 +0200

    Added parse'', parseTest'', eval and evalTest

--- Session4.hs
+++ Session4.hs
@@ -12,12 +12,10 @@ type Tree1a = BinTree Int Int
 type Tree1b = BinTree (Int, Int) (Int, Int) 
 type Tree1c = BinTree Unit Int
 type Tree4  = BinTree Int Unit
-
 --c
 pp :: (Show a, Show b) => BinTree a b -> RoseTree
 pp (Leaf x) = RoseNode (show x) []
 pp (Node x t1 t2) = RoseNode (show x) [pp t1, pp t2]
-
 --2a
 isDigit :: Char -> Bool
 isDigit x = x `elem` ['0'..'9']
@@ -88,7 +86,7 @@ fsaWS WS1  x | x == ' ' = WS1
 fsaWS _    _ = Error
 
 getFsa :: Char -> (State -> Char -> State)
-getFsa x | isDigit x            = fsaNum
+getFsa x | isDigit x ||x == '~' = fsaNum
          | isAlpha x            = fsaIdf
          | x `elem` "*^%<>+-/=" = fsaOps
          | x == '(' || x == ')' = fsaBra
@@ -100,24 +98,59 @@ tokenizer xs = allTokens xs []
 
 allTokens :: String -> [String] -> [String]
 allTokens "" lst = lst
-allTokens (x:xs) lst = allTokens rest (lst ++ [result])
+allTokens (x:xs) lst | result == [] = allTokens rest lst
+                     | otherwise = allTokens rest (lst ++ [result])
                        where (result, rest) = nextToken (getFsa x) Init "" (x:xs)
 
 nextToken :: (State -> Char -> State) -> State -> String -> String -> (String, String)
 nextToken fsa s t "" = (t, "")
 nextToken fsa s t (x:xs) = case result of
                             Error -> (t, (x:xs))
+                            WS1   -> ([], xs)
                             _     -> nextToken fsa result (t ++ [x]) xs
                            where 
                             result = fsa s x
 
-parse'' :: [String] ->  ((BinTree Char (Either Int Char)), String)
-parse'' [] = (Leaf (Left 0), "")
-parse'' (y:ys) | isDigit x = (Leaf (Left (read[x]::Int)), xs)
-               | isAlpha x = (Leaf (Right x), xs)
+
+convert :: String -> BinTree String (Either (Either Double Int) String)
+convert (x:xs) | x == '~' && '.' `elem` (x:xs) = Leaf (Left (Left ((negate $ read (xs))::Double)))
+               | x == '~' = Leaf (Left (Right ((negate $ read (xs))::Int)))
+               | '.' `elem` (x:xs) = Leaf (Left (Left ((read (x:xs))::Double)))
+               | otherwise = Leaf (Left (Right ((read (x:xs))::Int)))
+
+parse'' :: [String] ->  ((BinTree String (Either (Either Double Int) String)), [String])
+parse'' [] = (Leaf (Left (Right 0)), [""])
+parse'' (y:ys) | isDigit x || x == '~' = (convert y, ys)
+               | isAlpha x = (Leaf (Right y), ys)
                | x == '('  = ((Node op (t0) (t1)), tail r2) 
               where
                (x, xs) = (head y, tail y)
                (t0, r0) = parse'' ys
                (op, r1) = (head r0, tail r0)
-               (t1, r2) = parse'' [r1]       
+               (t1, r2) = parse'' r1
+
+parseTest'' = showRoseTree $ pp $ fst $ parse'' $ tokenizer "(((( a + b ) * (3.0^(22/11))) - abc) - ((1223//1000) * 100))"
+
+lookUp :: String -> Double
+lookUp "a" = 0.2
+lookUp "b" = 20.8
+lookUp "abc" = 10.0
+
+getFunction :: String -> (Double -> Double -> Double)
+getFunction "*" = (*)
+getFunction "^" = (**)
+getFunction "+" = (+)
+getFunction "-" = (-)
+getFunction "/" = (/)
+getFunction "//"= (\x y -> fromIntegral(round(x/y)))
+
+eval :: String -> Double
+eval xs = eval' $ fst $ parse'' $ tokenizer xs
+eval' :: BinTree String (Either (Either Double Int) String) -> Double
+eval' (Leaf (Left (Left  a))) = a
+eval' (Leaf (Left (Right a))) = fromIntegral(a)
+eval' (Leaf (Right a))        = lookUp a
+eval' (Node op t1 t2)         = f (eval' t1) (eval' t2)
+                                where f = getFunction op
+
+evalTest = eval "(((( a + b ) * (3.0^(22/11))) - abc) - ((1223//1000) * 100))"
